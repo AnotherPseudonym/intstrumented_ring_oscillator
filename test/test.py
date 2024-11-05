369b3a1c5d2a9a55a9f62803796f262d71587505
Jeremy <jeremy.mickelsen@gmail.com>
2024-11-04 20:02:31 -0800

    Removing the test sequence that lets the ring oscillator run - it takes too much processing and doesn't have a specific output goal anyway.

--- test/test.py
+++ test/test.py
@@ -34,11 +34,11 @@ async def test_project(dut):
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    #assert dut.uo_out.value == 0xAB
+    assert dut.uo_out.value == 0xAB
 
     dut.ui_in.value = 1
 
-    await ClockCycles(dut.clk, 1)
+    #await ClockCycles(dut.clk, 1)
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
